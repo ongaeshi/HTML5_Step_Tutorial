0e19b33e504b7fbfd88041dee5a06efac015ac29
ongaeshi <ongaeshi@example.com>
2011-01-23 23:28:15 +0900

    init commit.

---
+++ ContextHTML/ContextHTML_2_files/ContextHTML_2.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.2 local-build-20110123
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('ContextHTML_2_files/ContextHTML_2Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
---
+++ ContextHTML/ContextHTML_files/ContextHTML.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.2 local-build-20110123
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('ContextHTML_files/ContextHTMLMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
